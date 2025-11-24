2eb8711a47186123c8c740117eacb213d34abc47
Lady-Byron <samantha-everett@outlook.com>
2025-11-23 21:30:38 -0500

    Update register.ts

--- js/src/common/register.ts
+++ js/src/common/register.ts
@@ -1,20 +1,16 @@
-// js/src/common/register.ts
-
 import type Application from 'flarum/common/Application';
 import Widgets from 'flarum/extensions/afrux-forum-widgets-core/common/extend/Widgets';
-
 import TrendsWidget from './components/TrendsWidget';
-import { extName } from './extName';
 
 export default function registerWidget(app: Application) {
   new Widgets()
     .add({
-      key: 'liplum-trends-widget',
+      key: 'lady-byron-trends-widget',
       component: TrendsWidget,
       isDisabled: false,
       isUnique: true,
       placement: 'top',
       position: 1,
     })
-    .extend(app, extName);
+    .extend(app, 'lady-byron-trends');
 }
